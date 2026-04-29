1d6ceaf95e5fd3ada955e86d1cc854431a8bec39
Suraj Setty <surajmailhere@gmail.com>
2026-04-29 18:31:28 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="h-screen flex flex-col items-center justify-center bg-black text-white text-center">
+      <h1 className="text-4xl tracking-widest">LITE SOCIETY</h1>
+
+      <p className="mt-4 text-sm opacity-70">
+        Private network. Curated connections.
+      </p>
+
+      <p className="mt-8 text-xs opacity-50">
+        Website under development
+      </p>
+    </main>
+  );
+}
